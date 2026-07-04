d3b156b0a09abe0b0f9e3e0c57d3b62102b12e11
KirillllSo2 <128968334+KirillllSo2@users.noreply.github.com>
2026-07-04 14:55:09 +0700

    fuck forgot the mod id

--- src/main/java/com/example/examplemod/PenumbraDelight.java
+++ src/main/java/com/example/examplemod/PenumbraDelight.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ExampleMod.MOD_ID)
 public class ExampleMod {
-    public static final String MOD_ID = "examplemod";
+    public static final String MOD_ID = "penumb_delight";
     public static DelightApi addon;
 
     public ExampleMod(FMLJavaModLoadingContext context) {
